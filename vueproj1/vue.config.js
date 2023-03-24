0143c252de4cf4aaea26a6ae381ff9f3066d6100
Qinhao Xie <xqh1997@qq.com>
2023-03-24 20:53:13 +0000

    new nginx config and router and vue.config.js:add host and port config

--- vueproj1/vue.config.js
+++ vueproj1/vue.config.js
@@ -7,5 +7,16 @@ module.exports = defineConfig({
 module.exports = {
   devServer: {
     allowedHosts: "all",
+    host: '0.0.0.0',
+    // https:true,
+      port: 8080,
+      client: {
+        webSocketURL: 'ws://0.0.0.0:8080/ws',
+      },
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      }
+
+
   },
 };
